b83581ea0e3dcd3f8df255dc852802b45c7965a3
mob <lsh954325@gmail.com>
2024-07-27 21:54:08 +0900

    체력 인터페이스 추가

---
+++ Assets/Scripts/Interface/IHealth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 캐릭터 체력 인터페이스
+/// </summary>
+public interface IHealth
+{
+    /// <summary>
+    /// 캐릭터 현재 체력
+    /// </summary>
+    public int Hp { get; set; }
+
+    /// <summary>
+    /// 캐릭터 최대 체력
+    /// </summary>
+    public int MaxHp { get; set; }
+
+    /// <summary>
+    /// 피격시 실행되는 함수
+    /// </summary>
+    public void OnHit();
+
+    /// <summary>
+    /// 사망시 실행되는 함수
+    /// </summary>
+    public void OnDie();
+}
